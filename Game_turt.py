3602258df32bb723316238f630eea201c3afcb74
Sam <issam762@gmail.com>
2022-03-10 21:25:20 -0500

    Update Game_turt.py

--- Game_turt.py
+++ Game_turt.py
@@ -1,21 +1,21 @@
-# Python Program of a Turtle
+# Python Turtle Program
 import turtle
 
 shelly = turtle.Turtle()
 shelly.shape('turtle')
 
-# Coloring the turtle
+# Coloring the Turtle
 shelly.begin_fill()
 colors = ["red", "green", "blue"]
 shelly.color(colors[1])
 
-# Move the turtle using the FOR loop - Outer Loop Repeats the Square 6 times
+# Move the Turtle Using the FOR Loop - Outer Loop Repeats the Square 6 Times
 for n in range(6):
-    # Inner Loop Repeat 4 times to make a square
+    # Inner Loop Repeat 4 Times To Make a Square
     for i in range(4):
         shelly.forward(100)
         shelly.left(90)
-    # Add a turn before the next square
+    # Add a Turn Before the Next Square
     shelly.right(60)
 
 shelly.end_fill()
